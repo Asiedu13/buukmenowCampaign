a2c7fbd41fd45b3ea2ed6b53abe9894d7d6a9597
prince <princekofasiedu@gmail.com>
2023-09-28 20:08:30 +0000

    reduced mini-nav width

--- resources/views/livewire/campaign.blade.php
+++ resources/views/livewire/campaign.blade.php
@@ -1,12 +1,12 @@
 <div class="h-[900px]">
-    <header class="w-[234px] h-[56px] mt-[32 px]">
+    <header class="w-[234px] h-[56px]"
         <h2 class="font-medium text-xl">Customers</h2>
         <p class="text-sm font-normal pt-[16px] text-[#808785]
 ]">See all your customers in one place</p>
     </header>
 
 
-    <nav class="h-[50px] w-[1050px] mt-[45px] flex  border-b-2 ">
+    <nav class="h-[50px] w-[1040px] mt-[45px] flex border-b-2 ">
         <button wire:click="showCustomerLog" class="w-[106px] h-[50px] text-sm text-[#808785]  flex justify-center mr-[12px] hover:border-b-2 border-[#00100B] 
         hover:text-[#00100B] focus:border-b-2 border-[#00100B] focus:text-[#00100B]">
         Customer Log
